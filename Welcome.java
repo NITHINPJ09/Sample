cd27ec4b06ccf28f97d9890992d574e11790bd4e
Nithin Prince John <nithinpj09@gmail.com>
2023-06-08 16:10:46 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,7 +3,7 @@ public class Welcome
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Jenkins!");
-		System.out.println("CELL 1");
+		System.out.println("MY CELL 1");
 		System.out.println("CELL 2");
 		System.out.println("CELL 3");
 	}
